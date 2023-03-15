6b8e6b6662f8175d481ed51b64cf34c447d8479a
Gabriel Miguel Dias <112181260+gabrielmigueldias@users.noreply.github.com>
2023-03-14 21:06:54 -0300

    Create 0_variaveis.dart

---
+++ 0_variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Olá Mundo");
+  
+  String nome= "Gabriel Miguel Dias"; //representa uma variavel texto
+  int idade= 23;
+  double altura= 1.70;
+  bool fumante= false;
+  
+  print("Nome: $nome");
+  print("Idade: $idade");
+  print("Altura: $altura");
+  print("fuma? $fumante");  
+  
+}
